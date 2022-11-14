c75a184663f75de8e9cac677cfe4fe4172575d45
Caleb Geyer <caleb@geyer.ca>
2022-11-13 21:10:48 -0500

    Modified Courses.java to prepare for use by the new Instructor methods.

--- SEG2105Project/app/src/main/java/com/example/virtualclassroom/Courses.java
+++ SEG2105Project/app/src/main/java/com/example/virtualclassroom/Courses.java
@@ -1,8 +1,15 @@
 package com.example.virtualclassroom;
 
 public class Courses{
+    // Admin attributes
     private String courseCode;
     private String courseName;
+
+    // Instructor attributes
+    private String courseDays;
+    private String courseHours;
+    private String courseDescription;
+    private int courseStudentCapacity;
 
     //Constructors
     public Courses(){
@@ -21,6 +28,10 @@ public class Courses{
     public String getCourseName(){
         return courseName;
     }
+    public String getCourseDays() { return courseDays; }
+    public String getCourseHours() { return courseHours; }
+    public String getCourseDescription() { return courseDescription; }
+    public int getCourseStudentCapacity() {return courseStudentCapacity; }
     //setters
     public void setCourseCode(String code){
         courseCode = code;
@@ -28,5 +39,9 @@ public class Courses{
     public void setCourseName(String name){
         courseName = name;
     }
+    public void setCourseDays(String days) { courseDays = days; }
+    public void setCourseHours(String hours) { courseHours = hours; }
+    public void setCourseDescription(String description) { courseDescription = description; }
+    public void setCourseStudentCapacity(int capacity) { courseStudentCapacity = capacity; }
 
 }
