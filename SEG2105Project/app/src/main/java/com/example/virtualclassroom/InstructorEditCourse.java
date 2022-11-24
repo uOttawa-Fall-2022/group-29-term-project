9b9d05124de9e2ca392c7ba594a10af0053d2fe5
Caleb Geyer <caleb@geyer.ca>
2022-11-24 12:03:22 -0500

    Finished instructor homepage and editCourse activities.

--- SEG2105Project/app/src/main/java/com/example/virtualclassroom/InstructorEditCourse.java
+++ SEG2105Project/app/src/main/java/com/example/virtualclassroom/InstructorEditCourse.java
@@ -2,6 +2,7 @@ package com.example.virtualclassroom;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.widget.Button;
 import android.widget.EditText;
@@ -14,6 +15,9 @@ public class InstructorEditCourse extends AppCompatActivity {
     EditText updateCourseInfo;
     TextView courseCode, courseName, courseDays, courseHours, courseDescription,
             courseCapacity, courseInstructor;
+
+    Courses course;
+    String instructorName;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,49 +43,86 @@ public class InstructorEditCourse extends AppCompatActivity {
 
         CourseDB db = new CourseDB(InstructorEditCourse.this);
 
+        Intent intent = getIntent();
+        course = db.findCourseByName(intent.getStringExtra("course_name"));
+        instructorName = intent.getStringExtra("instructor_name");
+
+        setCourseViewerInfo();
+
         editDays.setOnClickListener(view ->{
-            if(editDays(courseName.getText().toString(),updateCourseInfo.getText().toString())){
-                courseDays.setText(updateCourseInfo.getText().toString());
+            if(!course.getInstructor().equals(instructorName)){
+                Toast.makeText(InstructorEditCourse.this,"Error: not instructor for this course.",Toast.LENGTH_SHORT).show();
+            }
+            else if(editDays(courseName.getText().toString(),updateCourseInfo.getText().toString())){
+                String tmp = "Course days: "+updateCourseInfo.getText().toString();
+                courseDays.setText(tmp);
                 updateCourseInfo.setText("");
                 Toast.makeText(InstructorEditCourse.this,"Course Updated",Toast.LENGTH_SHORT).show();
             }
         });
 
         editHours.setOnClickListener(view ->{
-            if(editHours(courseName.getText().toString(),updateCourseInfo.getText().toString())){
-                courseHours.setText(updateCourseInfo.getText().toString());
+            if(!course.getInstructor().equals(instructorName)){
+                Toast.makeText(InstructorEditCourse.this,"Error: not instructor for this course.",Toast.LENGTH_SHORT).show();
+            }
+            else if(editHours(courseName.getText().toString(),updateCourseInfo.getText().toString())){
+                String tmp = "Course hours: "+updateCourseInfo.getText().toString();
+                courseHours.setText(tmp);
                 updateCourseInfo.setText("");
                 Toast.makeText(InstructorEditCourse.this,"Course Updated",Toast.LENGTH_SHORT).show();
             }
         });
         editDescription.setOnClickListener(view ->{
-            if(editDescription(courseName.getText().toString(),updateCourseInfo.getText().toString())){
-                courseDescription.setText(updateCourseInfo.getText().toString());
+            if(!course.getInstructor().equals(instructorName)){
+                Toast.makeText(InstructorEditCourse.this,"Error: not instructor for this course.",Toast.LENGTH_SHORT).show();
+            }
+            else if(editDescription(courseName.getText().toString(),updateCourseInfo.getText().toString())){
+                String tmp = "Description: "+updateCourseInfo.getText().toString();
+                courseDescription.setText(tmp);
                 updateCourseInfo.setText("");
                 Toast.makeText(InstructorEditCourse.this,"Course Updated",Toast.LENGTH_SHORT).show();
             }
         });
         editCapacity.setOnClickListener(view ->{
-            if(editCapacity(courseName.getText().toString(),Integer.parseInt(updateCourseInfo.getText().toString()))){
-                courseCapacity.setText(updateCourseInfo.getText().toString());
-                updateCourseInfo.setText("");
-                Toast.makeText(InstructorEditCourse.this,"Course Updated",Toast.LENGTH_SHORT).show();
+            try {
+                if (!course.getInstructor().equals(instructorName)) {
+                    Toast.makeText(InstructorEditCourse.this, "Error: not instructor for this course.", Toast.LENGTH_SHORT).show();
+                } else if (editCapacity(courseName.getText().toString(), Integer.parseInt(updateCourseInfo.getText().toString()))) {
+                    String tmp = "Capacity: " + updateCourseInfo.getText().toString();
+                    courseCapacity.setText(tmp);
+                    updateCourseInfo.setText("");
+                    Toast.makeText(InstructorEditCourse.this, "Course Updated", Toast.LENGTH_SHORT).show();
+                }
+            }catch(Exception e){
+                Toast.makeText(InstructorEditCourse.this,"Error while changing capacity.",Toast.LENGTH_SHORT).show();
             }
         });
         unAssign.setOnClickListener(view ->{
             if(unAssignInstructor(courseName.getText().toString())){
+                String none = "NONE";
+                String none1 = "Course days: "+none;
+                String none2 = "Course hours: "+none;
+                String none3 = "Description: "+none;
+                String none4 = "Instructor: "+none;
+                String none5 = "Capacity: 0";
                 updateCourseInfo.setText("");
-                courseDays.setText(updateCourseInfo.getText().toString());
-                courseHours.setText(updateCourseInfo.getText().toString());
-                courseDescription.setText(updateCourseInfo.getText().toString());
-                courseInstructor.setText(updateCourseInfo.getText().toString());
-                courseCapacity.setText(updateCourseInfo.getText().toString());
+                courseDays.setText(none1);
+                courseHours.setText(none2);
+                courseDescription.setText(none3);
+                courseInstructor.setText(none4);
+                courseCapacity.setText(none5);
                 Toast.makeText(InstructorEditCourse.this,"Unassigned instructor from course",Toast.LENGTH_SHORT).show();
+            }else{
+                Toast.makeText(InstructorEditCourse.this,"Error occurred while unassigning from course.",Toast.LENGTH_SHORT).show();
             }
         });
         assign.setOnClickListener(view ->{
-            if(false){        //assignInstructor(courseName.getText().toString())){
+            if(!course.getInstructor().equals("NONE") && assignInstructor(courseName.getText().toString(),instructorName)){
+                String tmp = "Course days: "+updateCourseInfo.getText().toString();
+                courseInstructor.setText(tmp);
                 Toast.makeText(InstructorEditCourse.this,"Assigned instructor to course",Toast.LENGTH_SHORT).show();
+            }else{
+                Toast.makeText(InstructorEditCourse.this,"Error: course already has an instructor.",Toast.LENGTH_SHORT).show();
             }
         });
     }
@@ -109,11 +150,29 @@ public class InstructorEditCourse extends AppCompatActivity {
     public boolean unAssignInstructor(String courseName) {
         CourseDB db = new CourseDB(InstructorEditCourse.this);
         boolean days,hours,desc,cap,ins;
-        ins=db.setCourseInstructor(courseName,null);
-        days=db.setCourseDays(courseName,null);
-        hours=db.setCourseHours(courseName,null);
-        desc=db.setCourseDescription(courseName,null);
-        cap= db.setCourseCapacity(courseName,-1);
+        ins=db.setCourseInstructor(courseName,"NONE");
+        days=db.setCourseDays(courseName,"NONE");
+        hours=db.setCourseHours(courseName,"NONE");
+        desc=db.setCourseDescription(courseName,"NONE");
+        cap= db.setCourseCapacity(courseName,0);
         return(ins&&days&&hours&&cap&&desc);
+    }
+
+    private void setCourseViewerInfo(){
+        String code = "Course code: "+course.getCourseCode();
+        String name = "Course name: "+course.getCourseName();
+        String days = "Course name: "+course.getCourseDays();
+        String hours = "Course name: "+course.getCourseHours();
+        String desc = "Course name: "+course.getCourseDescription();
+        String cap = "Course name: "+course.getCourseStudentCapacity();
+        String ins = "Course name: "+course.getInstructor();
+
+        courseCode.setText(code);
+        courseName.setText(name);
+        courseDays.setText(days);
+        courseHours.setText(hours);
+        courseDescription.setText(desc);
+        courseCapacity.setText(cap);
+        courseInstructor.setText(ins);
     }
 }
