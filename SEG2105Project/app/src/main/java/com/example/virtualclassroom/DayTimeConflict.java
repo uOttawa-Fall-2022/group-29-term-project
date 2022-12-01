372420c78e47ca941135f8e242746325a4eee0d7
Caleb Geyer <caleb@geyer.ca>
2022-11-30 21:52:42 -0500

    Modified DayTimeConflict.java to handle nulls.

--- SEG2105Project/app/src/main/java/com/example/virtualclassroom/DayTimeConflict.java
+++ SEG2105Project/app/src/main/java/com/example/virtualclassroom/DayTimeConflict.java
@@ -1,8 +1,11 @@
 package com.example.virtualclassroom;
 
-public class Day_Time_Conflict {
+public class DayTimeConflict {
 
     public static boolean dayTimeConflict(Courses course1, Courses course2){
+        if(course1==null || course2==null || course1.getCourseHours() == null || course1.getCourseDays()==null || course2.getCourseDays()==null || course2.getCourseHours()==null){
+            return false;
+        }
         return (course1.getCourseDays().equals(course2.getCourseDays()) && course1.getCourseHours().equals(course2.getCourseHours()));
     }
 }
